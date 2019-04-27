f1731beb8557d78f6ba415591fa39119dc4abd4c
ayon <kr_ayon@icloud.com>
2019-04-27 09:48:36 +0200

    Initial Commit

---
+++ TabBarCoordinatePattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarCoordinatePattern
+//
+//  Created by krAyon on 27.04.19.
+//  Copyright © 2019 DocDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
